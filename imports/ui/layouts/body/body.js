12102c839f09c8dde7af1f97a5a703392d0c4f2b
Nick Florin <nickflorin@Nicks-iMac.local>
2017-08-05 15:11:24 -0400

    Grids

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,2 +1,11 @@
 import './body.html';
+import './body.css';
 import '../../components/grid/grid.html';
+
+Template.App_body.onCreated(function () {
+   $(document).on('scroll', function() {
+   	 $('.main-header').addClass('on-scroll')
+	   console.log('scrolling'); // you *really* don't want to alert in a scroll
+	   // $fixedHeader.css("top", getScrollTop()); 
+	});
+});
